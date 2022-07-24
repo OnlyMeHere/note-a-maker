639c84e8c5f5c1605adeb836604ccae7750b6287
OnlyMeHere <bennett45.j.m@gmail.com>
2022-07-23 19:54:57 -0700

    add routes and functions

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const express = require('express');
+// const fs = require('fs');
+// const express = require('express');
+
 
 let noteTitle;
 let noteText;
--- server.js
+++ server.js
@@ -1,15 +1,64 @@
-const { application } = require('express');
+// Calls middleware to server.js
+const fs = require('fs');
 const express = require('express');
-const api = require('./routes/index.js');
+const path = require('path');
+const api = require('/index.js');
+const { randomUUID } = require('crypto');
+
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
-app.use('api', api);
+app.use('/api', api);
 
 // middleware to point to the public directory
 app.use(express.static('public'));
 
+// GET route for notes from client to db
+app.get('/', (req, res) => {
+    
+    res.sendFile(__dirmane = '/public/notes.html')
+})
 
+// POST route for notes to db to client
+
+app.get('/notes', (req, res) => {
+    
+    res.sendFile(__dirname + 'public/notes.html')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+app.post('/api/notes', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if ( title && text ) {
+        const newNote = {
+            title: title,
+            text: text,
+            note_id randomUUID(),
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response)
+        db.push(newNote)
+
+        fs.writeToFile('./db/db.json', JSON.stringify(db), (err) => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    }
+}
+
+app.listen(PORT);
